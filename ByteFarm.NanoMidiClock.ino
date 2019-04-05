5fdebe4d45cb7a335d67bf618474c501fbc33b6f
John Diss <john.diss@newgrove.com>
2019-04-05 20:37:06 +0100

    interrupts for buttons

--- ByteFarm.NanoMidiClock.ino
+++ ByteFarm.NanoMidiClock.ino
@@ -4,14 +4,19 @@
 	Author:     RALPH\John
 */
 
+#include <splash.h>
+#include <Adafruit_SSD1306.h>
+#include <gfxfont.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_GFX.h>
+#include <fixedWidthFont.h>
 #include "Encoder.h"
-#include "UIButtonStates.h"
+//#include "UIButtonStates.h"
 #include <Arduino.h>
 #include <uClock.h>
-#include <LiquidCrystal_I2C.h>
-#include "Button.h"
+//#include "Button.h"
 #include <avr/interrupt.h>
-//#include <interrupt.h>
 
 
 using namespace umodular::clock;
@@ -27,17 +32,17 @@ using namespace umodular::clock;
 #define EncoderCLKPin 11
 #define EncoderDTPin 12
 
-bool IsPlaying = false;
+volatile bool IsPlaying = false;
 
 long long nextUIUpdate = 0;
 
-#define UIUpdateMillis 50
+#define UIUpdateMillis 30
 
 volatile uint16_t Tempo = 126;
 
 
 // The callback function which will be called by Clock each Pulse of 96PPQN clock resolution.
-void ClockOut96PPQN(uint32_t * tick)
+void ClockOut96PPQN(uint32_t* tick)
 {
 	// Send MIDI_CLOCK to external gears
 	Serial.write(MIDI_CLOCK);
@@ -56,45 +61,65 @@ void onClockStop()
 }
 
 
+//LiquidCrystal_I2C lcd(0x20, 16, 2);
+
+#define OLED_ADDR 0x3C
+Adafruit_SSD1306 display(-1);
 
 
+//Button PlayButton(PlaySwitchPin);
+//Button StopButton(StopSwitchPin);
+//Button EncoderButton(EncoderSwitchPin);
 
-LiquidCrystal_I2C lcd(0x20, 16, 2);
-
-Button PlayButton(PlaySwitchPin);
-Button StopButton(StopSwitchPin);
-Button EncoderButton(EncoderSwitchPin);
+volatile int8_t EncoderDelta = 0;
 
 void TempoChange(int8_t delta)
 {
-	Tempo += delta;
-	uClock.setTempo(Tempo);
+	EncoderDelta += delta;
+
+	int8_t mod = digitalRead(EncoderSwitchPin) == LOW ? 16 : 4;
+
+	if (EncoderDelta % mod == 0)
+	{
+		uClock.setTempo(Tempo += EncoderDelta / mod);
+		EncoderDelta = 0;
+	}
 }
 
 Encoder ENC(TempoChange);
 
 
+void InitOLED()
+{
+	display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+	display.clearDisplay();
+	display.setTextSize(2);
+	display.setTextColor(WHITE);
+}
 
 void setup()
 {
 	pinMode(EncoderCLKPin, INPUT_PULLUP);
 	pinMode(EncoderDTPin, INPUT_PULLUP);
+	pinMode(EncoderSwitchPin, INPUT_PULLUP);
+	pinMode(PlaySwitchPin, INPUT_PULLUP);
+	pinMode(StopSwitchPin, INPUT_PULLUP);
 
-	PCICR |= bit(PCIE0);     // set PCIE0 to enable PCMSK0 scan
-	PCMSK0 |= bit(PCINT3);   // set PCINT0 to trigger an interrupt on state change 
+
+	PCICR |= bit(PCIE0); // set PCIE0 to enable PCMSK0 scan
+	//PCMSK0 |= bit(PCINT0); // set PCINT0 to trigger an interrupt on state change 
+	PCMSK0 |= bit(PCINT1); // set PCINT0 to trigger an interrupt on state change 
+	PCMSK0 |= bit(PCINT2); // set PCINT0 to trigger an interrupt on state change 
+	PCMSK0 |= bit(PCINT3); // set PCINT0 to trigger an interrupt on state change 
 	PCMSK0 |= bit(PCINT4);
 
-	sei();  // turn on interrupts
+	sei(); // turn on interrupts
 
 
+	//InitLCD();
 
-	lcd.init();                      // initialize the lcd 
-  // Print a message to the LCD.
-	lcd.backlight();
-	lcd.setCursor(0, 0);
-	lcd.print("Midi Clock v0.1a");
+	InitOLED();
 
-	lcd.clear();
 	// Initialize serial communication at 31250 bits per second, the default MIDI serial speed communication:
 	Serial.begin(31250);
 
@@ -107,86 +132,97 @@ void setup()
 	uClock.setOnClockStopOutput(onClockStop);
 	// Set the clock BPM to 126 BPM
 	uClock.setTempo(Tempo);
-
-	// Starts the clock, tick-tac-tick-tac...
-	//uClock.start();
 }
 
-UIButtonStates ReadButtonStates()
+//UIButtonStates ReadButtonStates()
+//{
+//	const UIButtonStates states = {
+//		PlayButton.ReadButtonState(),
+//		StopButton.ReadButtonState(),
+//		EncoderButton.ReadButtonState() };
+//	return states;
+//}
+
+
+void DoUI()
 {
-	const UIButtonStates states = {
-		PlayButton.ReadButtonState(),
-		StopButton.ReadButtonState(),
-		EncoderButton.ReadButtonState() };
-	return states;
+	display.clearDisplay();
+
+	display.setCursor(0, 0);
+
+	display.print(IsPlaying ? "Running" : "Stopped");
+
+	display.setCursor(0, 32);
+	display.print("BPM ");
+
+	display.print(Tempo);
+
+	display.display();
 }
 
 
-
-void DoUI(const UIButtonStates buttons)
+void Play()
 {
-	lcd.setCursor(0, 0);
+	if (!IsPlaying)
+	{
+		uClock.start();
+		IsPlaying = true;
+	}
+}
 
-
-	uint8_t display = buttons.PlayButton | buttons.StopButton << 1 | buttons.EncoderButton << 2;
-
-	lcd.print(display);
-
-	lcd.print(" ");
-
-	lcd.print(IsPlaying ? "    Running" : "Not Running");
-
-	lcd.setCursor(0, 1);
-
-	lcd.print("BPM ");
-
-	lcd.print(Tempo);
-
-
-
+void Stop()
+{
+	if (IsPlaying)
+	{
+		uClock.stop();
+		IsPlaying = false;
+	}
 }
 
 void loop()
 {
-	const UIButtonStates buttons = ReadButtonStates();
-
-	if (buttons.PlayButton && buttons.StopButton)
-	{
-		//todo settings menu
-	}
-	else if (buttons.PlayButton)
-	{
-		if (!IsPlaying)
-		{
-			uClock.start();
-			IsPlaying = true;
-		}
-	}
-	else if (buttons.StopButton)
-	{
-		if (IsPlaying)
-		{
-			uClock.stop();
-			IsPlaying = false;
-		}
-	}
-
 	long long mi = millis();
 	if (mi > nextUIUpdate)
 	{
-		DoUI(buttons);
+		DoUI();
 		nextUIUpdate = mi + UIUpdateMillis;
+		delay(UIUpdateMillis);
 	}
-
+	
 }
 
-
+volatile uint8_t OldPINB = 0;
 
 ISR(PCINT0_vect)
 {
-	cli();
-	const uint8_t pinMask = 0b00011000;
-	const uint8_t pins = (PINB & pinMask) >> 3;
-	ENC.Update(pins);
-	sei();
+	const uint8_t changedPins = (PINB ^ OldPINB) & 0b00011111;
+
+	OldPINB = PINB;
+
+
+	const bool encoderChanged = (changedPins & 0b00011000) > 0;
+
+	const uint8_t start = (changedPins & 0b00000100) > 0 ? PINB & 0b00000100 : 0;
+
+	const uint8_t stop = (changedPins & 0b00000010) > 0 ? PINB & 0b00000010 : 0;
+
+
+	if (encoderChanged)
+	{
+		const uint8_t pinMask = 0b00011000;
+		const uint8_t pins = (PINB & pinMask) >> 3;
+		ENC.Update(pins);
+	}
+	//else if ((start | stop) > 0 && (PINB & 0b00000100) > 0 && (PINB & 0b00000010) > 0)
+	//{
+	//	//todo change mode
+	//}
+	else if (start > 0)
+	{
+		Play();
+	}
+	else if (stop > 0)
+	{
+		Stop();
+	}
 }
